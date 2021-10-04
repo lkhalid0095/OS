344f32617b792b1dcb91811f9e3c7e66b4663b9e
Lubna <lkhalid0095@bths.edu>
2021-10-03 23:02:16 -0400

    hw-1 complete

--- process.cpp
+++ process.cpp
@@ -1,26 +1,36 @@
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <iostream>
 #include <ctime>
 #include <string.h>
+#include <stdlib.h> 
 
+// Author: Lubna Khalid 
+//HW-1 PART ONE
 int main(){
 
-  using namespace std;
-
     int i=fork();
-
+    
+    //condition if it's a child process
     if(i == 0){
 
+          //gets the current day, date and time from the system
           time_t now = time(0);
           char*  date = ctime(&now);
+          //returns the string that came before the first space
+          // did this to just get the day out of the system
           char* day = strtok(date," ");
 
+          //prints the current day and the PID 
         printf ("Current Day of the Week is: %sday\nChild PID: %d\n",day, getpid());
     }
      
     else if (i> 0){
+      //Parent process
+      //prints the parent pid
       printf("Parent PID: %d\n",getpid());
     }
+    //terminate program
+    exit(1);
 }
