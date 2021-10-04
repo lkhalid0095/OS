4883fbda27e5ef79ca909bad686e39d1512b48b2
Lubna <lkhalid0095@bths.edu>
2021-10-04 00:13:56 -0400

    finished p1

--- process.cpp
+++ process.cpp
@@ -12,24 +12,19 @@ int main(){
        //gets the current day, date and time from the system
           time_t now = time(0);
           char*  date = ctime(&now);
+          int i=fork();
+          //condition if it's a child process
+         if(i == 0){
           //returns the string that came before the first space
-    int i=fork();
-    
-    //condition if it's a child process
-    if(i == 0){
-
-         
           // to get the substring of the day from string
           char* day = strtok(date," ");
 
           //prints the current day and the PID 
         printf ("Current Day of the Week is: %sday\nChild PID: %d\n",day, getpid());
-    }
-     
-    else if (i> 0){
+    } else if (i> 0){
       //Parent process
       //prints the shared date & parent pid
-        printf("Date: %s \nParent PID: %d\n", date, getpid());
+        printf("Current Date: %s Parent PID: %d\n", date, getpid());
     }
     //terminate program
     exit(1);
